daffde73018958ae0ebc6bb83fe3bc60a34950fb
David <david.sermoneta@hotmail.com>
2023-04-26 15:44:34 +0200

    Initial commit

---
+++ project.py
@@ -0,0 +1,30 @@
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+
+######DATA######
+
+# Velocity of water for different distances
+distance_from_plume = [0, 250, 500, 750, 1000]
+measured_velocity = [0.03, 0.032, 0.031, 0.027, 0.029]
+
+t_0 = -1.1 # temperature at the plume
+t_1000 = 1 # temperature 1000m away from the plume
+
+psu_0 = 34.1 # salinity at the plume
+psu_1000 = 35 # salinity 1000m away from the plume
+
+turbulence = 100
+
+
+
+################
+# EQUATION TO SOLVE:
+# k*y'' - u(x)*y' - u'(x)*y = 0
+
+
+# We use picewise cubic interpolation for the velocity data-points
+# Since we also need the derivative of the velocity later on linear interpolation isn't an option.
+velocity = sp.interpolate.CubicSpline(distance_from_plume, measured_velocity)
+velocity_p = velocity.derivative()
+
