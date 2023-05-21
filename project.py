a82d22e89fc88a81a309015ebb274b96771a8aa9
David <david.sermoneta@hotmail.com>
2023-05-21 17:07:23 +0200

    ><>

--- project.py
+++ project.py
@@ -29,36 +29,42 @@ velocity_p = velocity.derivative()
 x = 0
 
 
-
 def main():
     # uncomment these to test what happens if the velocity is constant
     #velocity = lambda _: 0.03
     #velocity_p = lambda _: 0
 
     N = 100
-    xs = np.linspace(0, 1000, N)
+    xs, h = np.linspace(0, 1000, N, retstep=True)
 
     velocities = list(map(velocity, xs))
-    velocity_ps = list(map(velocity_p, xs))
+    accelerations = list(map(velocity_p, xs))
 
-    _, ys_temperature_D, _ = ComputeNumericalSolution(velocity, velocity_p, N-2, t_0, t_1000) # Temperature
-    _, ys_salinity_D, _ = ComputeNumericalSolution(velocity, velocity_p, N-2, psu_0, psu_1000) # Salinity
+    _, ys_temperature_v1, _ = solve_ODE_v1(velocity, velocity_p, N-2, t_0, t_1000) # Temperature
+    _, ys_salinity_v1, _ = solve_ODE_v1(velocity, velocity_p, N-2, psu_0, psu_1000) # Salinity
 
-    ys_temperature_E = bvp_solve(velocity, velocity_p, t_0, t_1000, N)
-    ys_salinity_E = bvp_solve(velocity, velocity_p, psu_0, psu_1000, N)
+    ys_temperature_v2 = solve_ODE_v2(velocities, accelerations, N, h, t_0, t_1000)
+    ys_salinity_v2 = solve_ODE_v2(velocities, accelerations, N, h, psu_0, psu_1000)
 
-    fig, ax = plt.subplots(3,2)
+    ys_temperature_v3 = solve_ODE_v3(velocities, accelerations, N, h, t_0, t_1000)
+    ys_salinity_v3 = solve_ODE_v3(velocities, accelerations, N, h, psu_0, psu_1000)
+
+
+    fig, ax = plt.subplots(4,2)
     ax[0,0].plot(xs, velocities, label=f'v')
     ax[0,0].scatter(distance_from_plume, measured_velocity)
-    ax[0,1].plot(xs, velocity_ps, label=f'v\'')
-    ax[1,0].plot(xs, ys_temperature_D, label=f'Temperature N={N}')
-    ax[1,1].plot(xs, ys_salinity_D, label=f'Salinity (PSU) N={N}')
-    ax[2,0].plot(xs, ys_temperature_E, label=f'Temperature N={N}')
-    ax[2,1].plot(xs, ys_salinity_E, label=f'Salinity (PSU) N={N}')
+    ax[0,1].plot(xs, accelerations, label=f'v\'')
+    ax[1,0].plot(xs, ys_temperature_v1, label = f'Temperature N={N} v1', color = "green")
+    ax[1,1].plot(xs, ys_salinity_v1, label = f'Salinity (PSU) N={N} v1', color = "blue")
+    ax[2,0].plot(xs, ys_temperature_v2, label = f'Temperature N={N} v2', color = "purple")
+    ax[2,1].plot(xs, ys_salinity_v2, label = f'Salinity (PSU) N={N} v2', color = "orange")
+    ax[3,0].plot(xs, ys_temperature_v3, label = f'Temperature N={N} v3', color = 'red',)
+    ax[3,1].plot(xs, ys_salinity_v3, label = f'Salinity (PSU) N={N} v3', color ="black")
+
     fig.legend()
     plt.show()
-    print(ys_temperature_D)
 
+    fig, ax = plt.subplots(2)
     ys_density_D = density_approximation(ys_temperature_D, ys_salinity_D, xs)
     ys_density_E = density_approximation(ys_temperature_E, ys_salinity_E, xs)
 
@@ -70,9 +76,9 @@ def main():
     fig.legend()
     plt.show()
 
-
+    
 # Numerical solver of ODE. Still need to check if the entries are completely correct.
-def ComputeNumericalSolution(velocity, velocity_p, N , leftbc , rightbc ):
+def solve_ODE_v1(velocity, velocity_p, N , leftbc , rightbc ):
     h = ( 1000 ) / ( N + 1 )
     x = np . linspace (0 , 1000, N + 2 )
     A = np . zeros (( N , N ) )
@@ -110,34 +116,39 @@ def ComputeNumericalSolution(velocity, velocity_p, N , leftbc , rightbc ):
 
 
 
-def bvp_solve(velocity_f, velocity_fp, leftbc, rightbc, N):
-    A = assemble_A(velocity_f, velocity_fp, N)
+def solve_ODE_v2(velocities, accelerations, N, h, leftbc, rightbc):
+    A = assemble_A(velocities, accelerations, N, h)
     F = assemble_F(leftbc, rightbc, N)
-
-    print("v: ", velocity_f(300), velocity(699))
-    print("A:", A)
-    print("F", F)
-
     return np.linalg.solve(A, F)
 
 
 
-def assemble_A(velocity_f, velocity_fp, N):
-    xs, h = np.linspace(0, 1000, N, retstep=True)
+def solve_ODE_v3(velocities, velocity_ps, N, h, leftbc, rightbc):
+    A = assembly_of_A(N, h, turb, velocities, velocity_ps)
+    F = assembly_of_F(N, h, leftbc, rightbc)
+    return np.linalg.solve(A, F)
+
+
+
+def assemble_A(velocities, accelerations, N, h):
     h2 = h**2
 
     A = np.zeros((N, N))
     A[0,0] = 1
     A[N-1, N-1] = 1
     for i in range(1, N-1):
-        vel = velocity_f(xs[i])*h
-        velp = velocity_fp(xs[i])
+        #vel = velocity_f(xs[i])*h
+        #velp = velocity_fp(xs[i])*h
 
-        A[i, i-1] = (turb / h2) + (vel / (2*h))
-        A[i, i] =  (-2*turb / h2) - velp
-        A[i, i+1] = (turb / h2) - (vel / (2*h))
+        #A[i, i-1] = (turb / h2) + (vel / (2*h))
+        #A[i, i] =  (-2*turb / h2) - velp
+        #A[i, i+1] = (turb / h2) - (vel / (2*h))
 
-    return A
+        A [i , i - 1 ] = (1/2)*(2*turb + h*velocities[i])
+        A [i , i ] =  -2*turb + h2*accelerations[i]
+        A [i , i + 1 ] = (1/2)*(2*turb - h*velocities[i])
+
+    return (1/h2)*A
 
 
 
@@ -169,7 +180,33 @@ def density_approximation(temp, sal, xs):
         
 
 
+def assembly_of_A(N, h, turb, v, vp): # Finite Difference Scheme for second order d
+    h2 = h**2
+    A = np . zeros (( N , N ) )
 
+    A [0 , 0 ] = -2*turb + h2*vp[0]
+    A [0 , 1 ] = (1/2)*(2*turb - h*v[0])
+
+    for i in range (1 , N - 1 ) :
+        A [i , i - 1 ] = (1/2)*(2*turb + h*v[i])
+        A [i , i ] =  -2*turb + h2*vp[i]
+        A [i , i + 1 ] = (1/2)*(2*turb - h*v[i])
+
+
+    A [ N - 1 , N - 2 ] = (1/2)*(2*turb + h*v[N-1])
+    A [ N - 1 , N - 1 ] = -2*turb + h2*vp[N-1]
+
+    A = (1/h**2)*A
+    return A
+
+def assembly_of_F(N, h, leftbc, rightbc):
+    h2 = h**2
+    F = np.zeros(N)
+    F[0] = 0 - leftbc/h2
+    for i in range(1, N-1):
+        F[i] = 0
+    F[N-1] = 0 - rightbc/h2
+    return F
 
 
 if __name__ == "__main__":
